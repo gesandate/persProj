8bc715fc20466b0b43b50859315c107b5cef82d3
gesandate <gesandate2@miners.utep.edu>
2024-11-10 23:45:52 -0700

    Added the readFile method, still needs testing!!!!@

--- app.java
+++ app.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class app {
     public static void main (String [] args) {
-        
+        String [] lines = readFile("testFile");
+        for (String line : lines) { //python typa for loop
+            System.out.print(line);
+        }
     }
     public static boolean fileExists (String fileName) {
         File file = new File(fileName);
@@ -15,17 +18,30 @@ public class app {
         int count = 0;
 
         if (fileExists(fileName)) {
-            try (Scanner reader = new Scanner (new File(fileName))) {
+            try (Scanner reader = new Scanner (new File(fileName))) { //try with resources closes them when finished
                 while (reader.hasNext()) {
                     count++;
                     reader.nextLine();
                 }
             } catch (FileNotFoundException err) {
-                System.out.print("File " + fileName + " couldn't be found bro!");
+                System.out.println("File " + fileName + " couldn't be found bro!");
             }
         } else {
-            System.out.print("File doesn't exist bro!");
+            System.out.println("File doesn't exist bro!");
         }
         return count;
+    }
+
+    public static String[] readFile(String fileName) {
+        String [] lines = new String [numLines(fileName)];
+        try (Scanner reader = new Scanner(new File(fileName))) {
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = reader.nextLine();
+                reader.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be found yo!");
+        }
+        return lines;
     }
 }
