3af31af09be0c1c5bc2df190ce308f8f53f0e4ec
gesandate <gesandate2@miners.utep.edu>
2024-11-12 00:28:18 -0700

    Wrote file works, accountExists and isAlphanumeric methods added.

--- app.java
+++ app.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class app {
     public static void main (String [] args) {
-        String [] lines = readFile("testFile");
+        String [] lines = readFile("testFile.txt");
         for (String line : lines) { //python typa for loop
-            System.out.print(line);
+            System.out.println(line);
         }
     }
-    public static boolean fileExists (String fileName) {
+    public static boolean fileExists (String fileName) { // checks file existence
         File file = new File(fileName);
         return file.exists();
     }
 
-    public static int numLines (String fileName) {
+    public static int numLines (String fileName) { // counts lines in the file
         int count = 0;
 
         if (fileExists(fileName)) {
@@ -32,16 +32,40 @@ public class app {
         return count;
     }
 
-    public static String[] readFile(String fileName) {
+    public static String[] readFile(String fileName) { // converts the file into an array; each element is a line
         String [] lines = new String [numLines(fileName)];
         try (Scanner reader = new Scanner(new File(fileName))) {
             for (int i = 0; i < lines.length; i++) {
                 lines[i] = reader.nextLine();
-                reader.nextLine();
             }
         } catch (FileNotFoundException e) {
             System.out.println("File could not be found yo!");
         }
         return lines;
+    }
+
+    public static boolean accountExists(String accountName) { // checks account existence
+        return fileExists(accountName);
+    }
+
+    public static boolean isAlphanumeric(String input) { // checks if input is alphanumeric; number, uppercase, lowercase
+        boolean num = false;
+        boolean upp = false;
+        boolean low = false;
+        char curr;
+
+        for (int i = 0; i < input.length(); i++) {
+            curr = input.charAt(i);
+            if (Character.isDigit(curr)) {
+                num = true;
+            }
+            if (Character.isUpperCase(curr)) {
+                upp = true;
+            }
+            if (Character.isLowerCase(curr)) {
+                low = true;
+            }
+        }
+        return num && upp && low;
     }
 }
